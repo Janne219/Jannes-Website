35e22a4879843d0f36defcc3cac1cfdbdd66fa9e
Janne <janne.pruehs@studium.uni-hamburg.de>
2025-09-02 23:32:17 +0200

    Update Website

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function zeigeInfo() {
   const info = document.getElementById("info");
   info.style.display = info.style.display === "none" ? "block" : "none";
 }
+function toggleVorteile() {
+  const feld = document.getElementById("vorteile");
+  feld.style.display = (feld.style.display === "none") ? "block" : "none";
+}
